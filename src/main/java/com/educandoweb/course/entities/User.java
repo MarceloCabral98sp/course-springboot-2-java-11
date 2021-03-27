f4d7e7ccb060217a5583bcc95e97580daa7ea8ad
MarceloCabral98sp <marcelo.cabral98sp@gmail.com>
2021-03-27 02:27:31 -0300

    h2 database, test, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,15 +2,25 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
 // Serializable: transformar o obj em cadeia de bytes
+@Entity
 public class User implements Serializable {
 
 	// numero de série do Serializable
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
-	private String phone;
+	private String phone;	
 	private String password;
 	
 	// construtor vazio padrão
